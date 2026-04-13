c5b30439f62a6d659daf431d6aebc5ecc8faaefd
mecene phrygien <mecenephrygien@gmail.com>
2026-04-13 16:38:48 +0400

    live serach

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -13,6 +13,7 @@
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />-->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,200..800&display=swap" rel="stylesheet">
+
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
