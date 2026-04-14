aec4484734d1f7164ee9e50b2e45cd0c49cc8557
mecene phrygien <mecenephrygien@gmail.com>
2026-04-14 09:54:34 +0400

    test

--- resources/views/pages/orders/⚡create.blade.php
+++ resources/views/pages/orders/⚡create.blade.php
@@ -8,15 +8,17 @@ new class extends Component
 };
 ?>
 
-<div class="max-w-7xl">
+<div class="max-w-7xl mx-auto">
     <flux:breadcrumbs class="mb-5">
         <flux:breadcrumbs.item href="#">Commande</flux:breadcrumbs.item>
         <flux:breadcrumbs.item>Nouvelle</flux:breadcrumbs.item>
     </flux:breadcrumbs>
 
-    <!-- Heading + bouton -->
     <div class="flex items-center justify-between mb-6">
         <flux:heading size="xl" level="1">{{ __('Nouvelle Commande') }}</flux:heading>
     </div>
 
+    <flux:card>
+
+    </flux:card>
 </div>
