bb9ae2bcaab00fa1329d5e239371e40d7e14a2ba
mecene phrygien <mecenephrygien@gmail.com>
2026-04-24 13:42:36 +0400

    google fonts

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -13,7 +13,7 @@
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />-->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.js"></script>
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
