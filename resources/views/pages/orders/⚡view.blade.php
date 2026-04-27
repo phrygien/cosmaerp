a4692ef6121ee7c0fb83029731b1f02758488951
mecene phrygien <mecenephrygien@gmail.com>
2026-04-27 12:48:38 +0400

    View Details Orders

--- resources/views/pages/orders/⚡view.blade.php
+++ resources/views/pages/orders/⚡view.blade.php
@@ -9,5 +9,16 @@ new class extends Component
 ?>
 
 <div class="max-w-7xl">
-    {{-- Be present above all else. - Naval Ravikant --}}
+    <flux:breadcrumbs class="mb-5">
+        <flux:breadcrumbs.item href="#">Commande</flux:breadcrumbs.item>
+        <flux:breadcrumbs.item>Details</flux:breadcrumbs.item>
+    </flux:breadcrumbs>
+
+    <div class="flex items-center justify-between mb-6">
+        <flux:heading size="xl" level="1">{{ __('Précommande') }}</flux:heading>
+
+        <flux:button variant="primary" class="w-full sm:w-auto" href="{{ route('orders.create') }}" wire:navigate>
+            Bon de commande
+        </flux:button>
+    </div>
 </div>
