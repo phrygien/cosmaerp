1ed1733578e9f8b83a5bc1f7070584031c6cf8ee
mecene phrygien <mecenephrygien@gmail.com>
2026-04-27 15:50:54 +0400

    backgroud

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <x-layouts::app.sidebar :title="$title ?? null">
-    <flux:main>
+    <flux:main class="bg-slate-100 dark:bg-slate-900 min-h-screen">
         {{ $slot }}
     </flux:main>
 </x-layouts::app.sidebar>
