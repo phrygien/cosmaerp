6294535fee5828bc57adac3d51b913f67a666ed1
mecene phrygien <mecenephrygien@gmail.com>
2026-04-30 10:18:44 +0400

    New Icons

--- resources/views/layouts/app/sidebar2.blade.php
+++ resources/views/layouts/app/sidebar2.blade.php
@@ -2,7 +2,6 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
 <head>
     @include('partials.head')
-    <link rel="stylesheet" href="https://cdn.hugeicons.com/font/hgi-stroke-rounded.css" />
 </head>
 <body class="min-h-screen bg-white dark:bg-zinc-800 antialiased">
 <flux:sidebar sticky collapsible class="bg-zinc-50 dark:bg-zinc-900 border-r border-zinc-200 dark:border-zinc-700">
@@ -12,48 +11,41 @@
     </flux:sidebar.header>
 
     <flux:sidebar.nav>
-        <flux:sidebar.item :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>
-            <x-slot name="icon"><i class="hgi-stroke hgi-home-01"></i></x-slot>
+        <flux:sidebar.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>
             {{ __('Dashboard') }}
         </flux:sidebar.item>
 
         <flux:sidebar.spacer />
 
-        <flux:sidebar.group expandable expanded heading="Administration" class="grid">
-            <x-slot name="icon"><i class="hgi-stroke hgi-settings-01"></i></x-slot>
+        <flux:sidebar.group expandable expanded icon="adjustments-horizontal" heading="Administration" class="grid">
             <flux:sidebar.item href="{{ route('permissions') }}" wire:navigate>{{ __('Permissions') }}</flux:sidebar.item>
             <flux:sidebar.item href="{{ route('roles') }}" wire:navigate>{{ __('Roles') }}</flux:sidebar.item>
             <flux:sidebar.item href="{{ route('users') }}" wire:navigate>{{ __('Utilisateurs') }}</flux:sidebar.item>
         </flux:sidebar.group>
 
-        <flux:sidebar.group expandable expanded heading="Catalogues" class="grid">
-            <x-slot name="icon"><i class="hgi-stroke hgi-layers-01"></i></x-slot>
+        <flux:sidebar.group expandable expanded icon="rectangle-stack" heading="Catalogues" class="grid">
             <flux:sidebar.item href="{{ route('catalogue.marques') }}" wire:navigate>{{ __('Marques') }}</flux:sidebar.item>
             <flux:sidebar.item href="{{ route('catalogue.categories') }}" wire:navigate>{{ __('Categories') }}</flux:sidebar.item>
             <flux:sidebar.item href="{{ route('catalogue.parkod') }}" wire:navigate>{{ __('PARKOD') }}</flux:sidebar.item>
             <flux:sidebar.item href="{{ route('catalogue.products') }}" wire:navigate>{{ __('Produits') }}</flux:sidebar.item>
         </flux:sidebar.group>
 
-        <flux:sidebar.item :href="route('fournisseurs')" :current="request()->routeIs('fournisseurs')" wire:navigate>
-            <x-slot name="icon"><i class="hgi-stroke hgi-truck-01"></i></x-slot>
+        <flux:sidebar.item icon="users" :href="route('fournisseurs')" :current="request()->routeIs('fournisseurs')" wire:navigate>
             {{ __('Fournisseurs') }}
         </flux:sidebar.item>
 
-        <flux:sidebar.item :href="route('magasin')" :current="request()->routeIs('magasin')" wire:navigate>
-            <x-slot name="icon"><i class="hgi-stroke hgi-store-01"></i></x-slot>
+        <flux:sidebar.item icon="building-storefront" :href="route('magasin')" :current="request()->routeIs('magasin')" wire:navigate>
             {{ __('Dépôt') }}
         </flux:sidebar.item>
 
-        <flux:sidebar.group expandable expanded heading="Précommande" class="grid">
-            <x-slot name="icon"><i class="hgi-stroke hgi-shopping-cart-01"></i></x-slot>
+        <flux:sidebar.group expandable expanded icon="shopping-cart" heading="Précommande" class="grid">
             <flux:sidebar.item href="{{ route('orders.list') }}" wire:navigate>{{ __('Liste') }}</flux:sidebar.item>
             <flux:sidebar.item href="{{ route('orders.create') }}" wire:navigate>{{ __('Nouvelle') }}</flux:sidebar.item>
         </flux:sidebar.group>
 
         <flux:sidebar.spacer />
 
-        <flux:sidebar.group expandable expanded heading="Réception" class="grid">
-            <x-slot name="icon"><i class="hgi-stroke hgi-inbox-01"></i></x-slot>
+        <flux:sidebar.group expandable expanded icon="queue-list" heading="Réception" class="grid">
             <flux:sidebar.item href="{{ route('reception_commande.list') }}" wire:navigate>{{ __('Historique réception') }}</flux:sidebar.item>
             <flux:sidebar.item href="{{ route('reception_commande.create') }}" wire:navigate>{{ __('Nouvelle réception') }}</flux:sidebar.item>
         </flux:sidebar.group>
@@ -85,6 +77,7 @@
                             :name="auth()->user()->name"
                             :initials="auth()->user()->initials()"
                         />
+
                         <div class="grid flex-1 text-start text-sm leading-tight">
                             <flux:heading class="truncate">{{ auth()->user()->name }}</flux:heading>
                             <flux:text class="truncate">{{ auth()->user()->email }}</flux:text>
@@ -96,7 +89,7 @@
             <flux:menu.separator />
 
             <flux:menu.radio.group>
-                <flux:menu.item :href="route('profile.edit')" wire:navigate>
+                <flux:menu.item :href="route('profile.edit')" icon="cog" wire:navigate>
                     {{ __('Paramètres') }}
                 </flux:menu.item>
             </flux:menu.radio.group>
@@ -108,6 +101,7 @@
                 <flux:menu.item
                     as="button"
                     type="submit"
+                    icon="arrow-right-start-on-rectangle"
                     class="w-full cursor-pointer"
                     data-test="logout-button"
                 >
