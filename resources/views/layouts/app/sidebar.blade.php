8fcd984c5c4aa031bbcc0b23d2c96aba5a04222e
mecene phrygien <mecenephrygien@gmail.com>
2026-04-30 10:20:35 +0400

    New Icons

--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -52,8 +52,7 @@
 
         <flux:sidebar.spacer />
 
-        <flux:sidebar.group expandable expanded heading="Réception" class="grid">
-            <x-slot name="icon"><i class="hgi-stroke hgi-inbox-01"></i></x-slot>
+        <flux:sidebar.group expandable expanded heading="Réception" class="grid" icon="queue-list">
             <flux:sidebar.item href="{{ route('reception_commande.list') }}" wire:navigate>{{ __('Historique réception') }}</flux:sidebar.item>
             <flux:sidebar.item href="{{ route('reception_commande.create') }}" wire:navigate>{{ __('Nouvelle réception') }}</flux:sidebar.item>
         </flux:sidebar.group>
