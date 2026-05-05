51a1497edf05b60bca86364865d643aef7f3d5b9
mecene phrygien <mecenephrygien@gmail.com>
2026-05-05 17:07:11 +0400

    delete sanctum

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StockController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::post('/stock/webhook', [StockController::class, 'webhook'])
     ->middleware('webhook.verify');
